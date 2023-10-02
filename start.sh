332f0f4679ccd1ecbc784e2eaa2cb39aca6e62d1
AKR <98525798+Akrtechi@users.noreply.github.com>
2023-10-02 22:59:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Akrtechi/VikramTG.git /VikramTG
+  git clone https://github.com/Akrtechi/VikramTG2.git /VikramTG2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /VikramTG
+  git clone $UPSTREAM_REPO /VikramTG2
 fi
-cd /VikramTG
+cd /VikramTG2
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
